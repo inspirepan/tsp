74b4079852ba4218378be4bfdfd42da558a23c0c
PanJx <panjx6@outlook.com>
2020-10-22 13:03:31 +0800

    update GA's template

--- optimization-multiple-minimal-function/ga.py
+++ optimization-multiple-minimal-function/ga.py
@@ -4,33 +4,33 @@ from arguments import N
 
 class GeneticAlgorithms:
     def solve(self):
-        # 种群P
-        p = self.get_initial_generation(N)
-        # 当前代
-        k = 0
+        p = self.get_initial_generation(N)  # 种群P
+        k = 0  # 当前代
         s = 0
+        f_v = self.get_fitness_value(p)
         while s > 0:
             m = 0
             while m < N:
-                np.random.shuffle(p)
-                temp = p[:2]
+                temp = np.random.choice(p, 2)
                 if self.p_crossover(temp) > np.random.uniform(0, 1):
                     temp = self.crossover(temp)
                 mutated = self.mutation(temp)
+                p.extend(mutated)
                 m += 2
             k += 1
+            f_v = self.get_fitness_value(p)
 
     def get_fitness_value(self):
         pass
 
-    def get_initial_generation(self):
+    def get_initial_generation(self, temp):
         pass
 
-    def p_crossover(self):
+    def p_crossover(self, temp):
         pass
 
-    def crossover(self):
+    def crossover(self, temp):
         pass
 
-    def mutation(self):
+    def mutation(self, temp):
         pass
